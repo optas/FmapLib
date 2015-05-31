e1de2b9906b30f86a82913c4923ddaf5ff713f57
corman <etienne.corman@gmail.com>
2015-06-01 01:33:38 +0200

    Test WKS

--- src/demo_basics.m
+++ src/demo_basics.m
@@ -12,6 +12,10 @@ inmesh
 LB             = Laplace_Beltrami(inmesh);
 [evals, evecs] = LB.get_spectra(100, 'barycentric');
 
+%% Compute WKS for log sample
+variance = 6;
+[E, sigma] = Mesh_Feature.energy_sample_generator('log_linear', evals(1), evals(end), 10, variance);
+
 %% Save objects
     save('mesh_and_LB', 'refMesh', 'LB');
 
