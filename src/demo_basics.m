7b42b0ad24a8ecb5a9d40d1877ee598c10bb57d0
panos <optas@rescomp-13-277459.stanford.edu>
2015-06-02 23:44:02 -0700

    Clearing unnecessary code of Mesh.

--- src/demo_basics.m
+++ src/demo_basics.m
@@ -2,8 +2,8 @@
 %%  (Work in progress)
 
 %% Load a Mesh and calculate basic quantities.
-meshfile  = '../data/kid_rodola/0001.isometry.1.off';
-% shapeFile  = '/Users/optas/Dropbox/Matlab_Projects/3D_Meshes/Data/kid_rodola/0001.isometry.1.off';
+% meshfile  = '../data/kid_rodola/0001.isometry.1.off';
+meshfile  = '/Users/optas/Dropbox/Matlab_Projects/3D_Meshes/Data/kid_rodola/0001.isometry.1.off';
 inmesh    = Mesh(meshfile, 'rodola_1_1');
 inmesh.set_triangle_angles();
 inmesh.set_vertex_areas('barycentric');
@@ -19,11 +19,10 @@ LB             = Laplace_Beltrami(inmesh);
 %% WKS - HKS
 nsamples = 100;
 [energies, sigma] = Mesh_Features.energy_sample_generator('log_linear', evals(2), evals(end), nsamples);
-
 wks_sig           = Mesh_Features.wave_kernel_signature(evecs(:,2:end), evals(2:end), energies, sigma);
 
-hks_sig = Mesh_Features.heat_kernel_signature(evecs(:,2:end), evals(2:end), energies);
-
+hks_sig           = Mesh_Features.heat_kernel_signature(evecs(:,2:end), evals(2:end), energies);
+hks_sun           = Mesh_Features.hks_Sun(evecs(:,2:end), evals(2:end), energies);
 
 
 % wks_aubrey_sig    = Mesh_Features.wks_aubrey(evecs(:,2:end), evals(2:end), energies, sigma);  % Add to Unit Test.
