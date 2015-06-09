cca499bdcefd56bc2caff8d4d61333789c31a372
panos <optas@rescomp-13-277459.stanford.edu>
2015-06-08 21:46:16 -0700

    removing norm_v

--- src/Basic_Utils/normv.m
+++
@@ -1,9 +0,0 @@
-function n = normv(A)
-% Compute the euclidean norm of each row of A.
-%
-% Input    A - (m x n) matrix
-%
-% Output   n - (m x 1) vector, n(i) = ||A(i,:)||_2
-
-    n = sqrt(sum(A.^2, 2));
-end
