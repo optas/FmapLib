86df273248a80a39fd36b16a37bde3ed67909e7d
panos <optas@rescomp-13-277459.stanford.edu>
2015-09-25 02:04:14 -0700

    Update get_project_path for Vignesh

--- src/Basic_Utils/get_project_paths.m
+++ src/Basic_Utils/get_project_paths.m
@@ -15,7 +15,7 @@ function [data_path, code_path] = get_project_paths(project_name)
     data_path = '';
     code_path = '';
     
-    if strcmp(name(1:end-1), 'optasMacPro')
+    if strcmp(name(1:end-1), 'optasMacPro')                                                 % Panos's Space.
         if strcmp(project_name, 'FmapLib')
             data_path = '/Users/optas/Documents/Git_Repos/FmapLib/data/';
             code_path = '/Users/optas/Documents/Git_Repos/FmapLib/src/External_Code/';
@@ -23,15 +23,26 @@ function [data_path, code_path] = get_project_paths(project_name)
         elseif strcmp(project_name, 'ImageJointUnderstanding')
             data_path = '/Users/optas/Dropbox/With_others/Zimo_Peter_Panos/Joint_Image_Understanding/Data/';
             code_path = '/Users/optas/Dropbox/matlab_projects/External_Packages/';
+                
+        else strcmp(project_name, 'Shape_Classification')
+            data_path = '/Users/optas/Dropbox/Matlab_Projects/Shape_Classification/Data';
+            code_path = '/Users/optas/Dropbox/Matlab_Projects/Shape_Classification/src';
         end
+        
     elseif strcmp(name(1:end-1), 'orionp.stanford.edu')
         if strcmp(project_name, 'ImageJointUnderstanding')
             data_path = '/orions3-zfs/projects/optas/Matlab_projects/Data/ImageJointUnderstanding/';
             code_path = '';
-        end        
-    elseif strcmp(name(1:end-1), 'Etienne-HP')
+        end
+        
+    elseif strcmp(name(1:end-1), 'Etienne-HP')                                              % Etienne's space.
+        
         data_path = 'C:\Users\Etienne\Desktop\GitHubProj\FmapLib\data\';
         code_path = 'C:\Users\Etienne\Desktop\GitHubProj\FmapLib\src\External_Code\';
+    
+    elseif strcmp(name(1:end-1), 'Vignesh_Comp_Name')                                       % Vignesh's space.
+        data_path = '';
+        code_path = '';        
     
     else
         error('Unknown computer.');
