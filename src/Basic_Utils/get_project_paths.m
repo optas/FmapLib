7bab856bdac3c9ffb6af9f07b915370c68217489
panos <optas@rescomp-13-277459.stanford.edu>
2016-04-18 17:37:07 -0700

    white space

--- src/Basic_Utils/get_project_paths.m
+++ src/Basic_Utils/get_project_paths.m
@@ -1,14 +1,12 @@
 function [data_path, code_path] = get_project_paths(project_name)
-% Computes the personalized file paths for the data and external code libraries, assosiated with a specific project.
-% Input:
-%           project_name - (str) A string specifying the project's name.
-% Output:   
-%           data_path    - (str) File path to project's data repository.
-%           code_path    - (str) File path to project's (external) code repository.
+    % Computes the personalized file paths for the data and external code libraries, assosiated with a specific project.
+    % Input:
+    %           project_name - (str) A string specifying the project's name.
+    % Output:   
+    %           data_path    - (str) File path to project's data repository.
+    %           code_path    - (str) File path to project's (external) code repository.
 
-
-    name = get_computer_name();
-        
+    name = get_computer_name();        
     data_path = '';
     code_path = '';
     
@@ -26,25 +24,22 @@ function [data_path, code_path] = get_project_paths(project_name)
             code_path = '/Users/optas/Dropbox/Matlab_Projects/Shape_Classification/src/';
         end
         
-    elseif strcmp(name(1:5), 'orion') && strcmp(name(end-12:end), '.stanford.edu')          % Orion server farm on Stanford.        
+    elseif strcmp(name(1:5), 'orion') && strcmp(name(end-12:end), '.stanford.edu')          % Orion server farm on Stanford.
         if strcmp(project_name, 'FmapLib')
             data_path = '/orions3-zfs/projects/optas/Matlab_Projects/FmapLib/data/';
             code_path = 'orions3-zfs/projects/optas/Matlab_Projects/FmapLib/src/';
         
         elseif strcmp(project_name, 'Joint_Image_Analysis')
             data_path = '/orions3-zfs/projects/optas/Data/Joint_Image_Analysis/';
-            code_path = '/orions3-zfs/projects/optas/Matlab_Projects/External_Matlab_Code/';             
-        
+            code_path = '/orions3-zfs/projects/optas/Matlab_Projects/External_Matlab_Code/';                     
         end
         
-    elseif strcmp(name, 'Etienne-HP')                                                       % Etienne's space.        
+    elseif strcmp(name, 'Etienne-HP')                                                       % Etienne's space.
         data_path = 'C:\Users\Etienne\Desktop\GitHubProj\FmapLib\data\';
-        code_path = 'C:\Users\Etienne\Desktop\GitHubProj\FmapLib\src\External_Code\';
-        
-    else
-        
-        error('Unknown computer.');
-        
+        code_path = 'C:\Users\Etienne\Desktop\GitHubProj\FmapLib\src\External_Code\';            
+    
+    else        
+        error('Unknown computer.');        
     end
         
     if isempty(data_path)
